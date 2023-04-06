2cde59afb5d23ace8dde8f84a806e3b6ff6d0064
German Shvayukou <geras210@gmail.com>
2023-04-06 11:15:49 +0300

    add log out

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,17 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../config";
 import "./Header.css";
 function Header() {
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    signOut(auth)
+      .then(() => navigate("/sign-in"))
+      .catch((error) => {});
+  };
+
   return (
     <div className="header">
       <div className="header_title">
@@ -9,6 +20,9 @@ function Header() {
       <div className="icon_buttons">
         <img className="notification_icon" src="notification_icon.png" alt="" />
         <img className="menu_icon" src="menu_icon.png" alt="" />
+        <button className="btn-log-out" onClick={handleLogOut}>
+          Log out
+        </button>
       </div>
     </div>
   );
